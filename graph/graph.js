4a1b815671b7b9ef97d34d337098fd0983421f27
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-03-24 01:31:55 -0700

    changes to graph.js

--- graph/graph.js
+++ graph/graph.js
@@ -1,17 +1,19 @@
-async function testTensor (tensor, num_runs) {
+async function testInput (tensors, num_runs) {
   console.time("model loading time");
-  const model = await tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/final5/model.json");
+  const model = await tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/multipleoutputs/model.json");
   console.timeEnd("model loading time");
   console.time("first prediction");
-  const predictionsTensor =  await model.executeAsync([tensor]);
-  const predictions = predictionsTensor.dataSync();
+  const predictionsTensor =  await model.executeAsync(tensors);
+  debugger;
+  const predictions = predictionsTensor.ArraySync();
   console.timeEnd("first prediction");
   console.log("the predictions are", predictions);
+  download("results.json", predictions);
   let subsequent_times =new Float32Array(num_runs - 1);
   for (let i = 0; i < num_runs - 1 ; i++) {
     let begin= window.performance.now();
-    const predictionsTensor =  await model.executeAsync([tensor]);
-    const predictions = predictionsTensor.dataSync();
+    const predictionsTensor =  await model.executeAsync(tensors);
+    const predictions = predictionsTensor.ArraySync();
     let end= window.performance.now();
     let time = (end-begin) ;
     subsequent_times[i] = time;
@@ -20,23 +22,38 @@ async function testTensor (tensor, num_runs) {
   console.log("the average of the subequent predictions are", average(subsequent_times));
 }
 
+function download(filename, text) {
+    var element = document.createElement('a');
+
+    element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(text)));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
 function average(array) {
     let average = array.reduce((a, b) => a + b) / array.length;
     return average;
 }
 
-function testTensorDefininedInCode() {
-    let tensor = tf.ones([28, 28]);
-    tensor = tensor.expandDims(0);
-    console.log("the tensor is", tensor);
-    testTensor(tensor, 10);
+function testInputDefininedInCode() {
+    let tensor1 = tf.ones([5]);
+    tensor1 = tensor1.expandDims(0);
+    let tensor2 = tf.ones([5]);
+    tensor2 = tensor2.expandDims(0);
+    testInput([tensor1, tensor2], 10);
 }
 
 function testUploadedTensor(array) {
     let tensor = tf.tensor(array);
     tensor = tensor.expandDims(0);
     console.log("the tensor insider is test uploaded array is", tensor);
-    testTensor(tensor, 10);
+    testInput([tensor], 10);
 }
 
 
@@ -77,4 +94,4 @@ function readFileContent(file) {
 
 
 initializeFileUploader();
-//testTensorDefininedInCode();
+testInputDefininedInCode();
