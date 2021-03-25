ae295fda0f9326872d3bcdaedc37c4db322b7aeb
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-03-25 03:45:25 -0700

    new files

---
+++ graph/benchmark.js
@@ -0,0 +1,34 @@
+async function benchmarkInput (model_path, tensors, num_runs) {
+  console.time("model loading time");
+  const model = await tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/multipleoutputs3/model.json");
+  console.timeEnd("model loading time");
+  console.time("first prediction");
+  const predictions = await runModel(model, tensors, false);
+  console.timeEnd("first prediction");
+
+  let subsequent_times =new Float32Array(num_runs - 1);
+  for (let i = 0; i < num_runs - 1 ; i++) {
+    let begin= window.performance.now();
+    const predictions = await runModel(model, tensors, false);
+    let end= window.performance.now();
+    let time = (end-begin) ;
+    subsequent_times[i] = time;
+  }
+  console.log("subsequent predictions are in ms", subsequent_times);
+  console.log("the average of the subequent predictions are", average(subsequent_times));
+}
+
+function average(array) {
+    let average = array.reduce((a, b) => a + b) / array.length;
+    return average;
+}
+
+function benchmarkInputDefininedInCode() {
+    let tensor1 = tf.ones([5]);
+    tensor1 = tensor1.expandDims(0);
+    let tensor2 = tf.ones([5]);
+    tensor2 = tensor2.expandDims(0);
+    benchmarkInput("https://storage.googleapis.com/uplara_tfjs/multipleoutputs3/model.json", [tensor1, tensor2], 10);
+}
+
+benchmarkInputDefininedInCode();
--- graph/core.js
+++ graph/core.js
@@ -21,25 +21,7 @@ async function runModel(model, tensors, returnTensorReferences ) {
 
 
 
-async function benchmarkInput (tensors, num_runs) {
-  console.time("model loading time");
-  const model = await tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/multipleoutputs3/model.json");
-  console.timeEnd("model loading time");
-  console.time("first prediction");
-  const predictions = await runModel(model, tensors, false);
-  console.timeEnd("first prediction");
 
-  let subsequent_times =new Float32Array(num_runs - 1);
-  for (let i = 0; i < num_runs - 1 ; i++) {
-    let begin= window.performance.now();
-    const predictions = await runModel(model, tensors, false);
-    let end= window.performance.now();
-    let time = (end-begin) ;
-    subsequent_times[i] = time;
-  }
-  console.log("subsequent predictions are in ms", subsequent_times);
-  console.log("the average of the subequent predictions are", average(subsequent_times));
-}
 
 
 
@@ -57,18 +39,6 @@ function download(filename, text) {
     document.body.removeChild(element);
 }
 
-function average(array) {
-    let average = array.reduce((a, b) => a + b) / array.length;
-    return average;
-}
-
-function benchmarkInputDefininedInCode() {
-    let tensor1 = tf.ones([5]);
-    tensor1 = tensor1.expandDims(0);
-    let tensor2 = tf.ones([5]);
-    tensor2 = tensor2.expandDims(0);
-    benchmarkInput([tensor1, tensor2], 10);
-}
 
 function testUploadedTensor(array) {
     let tensor = tf.tensor(array);
@@ -77,4 +47,3 @@ function testUploadedTensor(array) {
     benchmarkInput([tensor], 10);
 }
 
-benchmarkInputDefininedInCode();
