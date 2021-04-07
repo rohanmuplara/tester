4ca0219ae6fa9d39d8558522b6baeb5ababa030c
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-04-06 20:36:51 -0700

    benchmark new

--- graph/benchmark_new.js
+++ graph/benchmark_new.js
@@ -23,22 +23,32 @@ function average(array) {
     return average;
 }
 
-function benchmarkInputDefininedInCode() {
+async function benchmarkInputDefininedInCode() {
    console.log("tf.loadGraphModeling time");
    console.time("tf.loadGraphModeling times");
-    let models = Promise.all([
+    let models = await Promise.all([
       tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/grapy/atr_512_256_mobilenet_edge_loss_1/tfjs/model.json.gz"),
       tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/tops/expseg/expected_seg_debug/tfjs/model.json.gz"),
     tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/tops/tps/short_tshirts_default/tfjs/model.json.gz"),
     tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/tops/tom/model_60/tfjs/model.json.gz"),
-    tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/tops/model_800/tfjs/model.json.gz"),
     tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/blazeface/tfjs/model.json.gz"),
     tf.loadGraphModel("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/densepose/densepose_js/model.json.gz")
     ])
-  console.timeEnd("tf.loadGraphModeling times);
-    let tensor1 = tf.ones([256, 256,3]);
-    tensor1 = tensor1.expandDims(0);
-    benchmarkInput("https://storage.googleapis.com/tfjs-alok-uplara-abcde/bottoms_gzip/blazeface/tfjs/model.json.gz", [tensor1], 100);
+    console.timeEnd("tf.loadGraphModeling times");
+    let grapyModel = models[0];
+    let segModel = models[1];
+    let tpsModel = models[2];
+    let tomModel = models[3];
+    let blazefaceModel = models[4];
+    let densposeModel = models[5];
+    let tensor1 = tf.ones([1, 256, 256,3]);
+    let grapyTensor = [tf.ones([1, 512, 256, 3])];;
+    let densposeTensor = [tf.ones([1, 256, 192, 3])];
+    let blazefaceTensor = [tf.ones([1, 256, 256, 3])];
+    let segTensor = [tensor1, tensor2, tensor3, tensor4, tensor5];
+
+   
+    benchmarkInput(models);
 }
 
 benchmarkInputDefininedInCode();
