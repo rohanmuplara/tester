c18e179e608f78e6d0b8d9efc3781e04c5e92a83
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-04-14 04:19:57 -0700

    stitched values

--- graph/stitched.js
+++ graph/stitched.js
@@ -1,35 +1,47 @@
-async function benchmarkStichedInput (models, num_runs) {
-    
+async function runModel(model, tensors, returnTensorReferences ) {
+    let num_outputs = model.outputs.length;
+    let string_array = ["Identity:0"]; 
+    for (let i = 1; i < num_outputs; i++) {
+      string_array.push("Identity_" + i +":0");
+    }
+    const predictionsTensor =  await model.executeAsync(tensors, string_array);
+    if (returnTensorReferences) {
+      return predictionsTensor;
+    } else {
+      if (Array.isArray(predictionsTensor)) {
+        const promises = predictionsTensor.map(x => x.array());
+        const arrayTensor = await Promise.all(promises);
+        tf.dispose(predictionsTensor);
+        return arrayTensor;
+      } else {
+        const arrayTensor = predictionsTensor.arraySync()
+        tf.dispose(predictionsTensor);
+        return arrayTensor;
+      }
+    }
+}
 
+async function benchmarkStichedInput (num_runs) {
+  
+    
     let model1 = await  tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/seperatemobilenet/model.json")
     let model2 = await  tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/seperatemobilenet4/model.json")
-    let tensor3 = [tf.ones([1,224, 224, 3])];
+    let tensor1 = [tf.ones([1,224, 224, 3])];
     let tensor2 = [tf.ones([1,224, 224, 3])];
   for  (let i = 0; i < num_runs; i++) {
-    console.time("model2  prediction" + i);
-    const predictions2 = await runModel(model2, tensor3, false);
-    console.timeEnd("model2  prediction" + i);
     console.time("model1  prediction" + i);
-    const predictions1 = await runModel(model1, tensor2, false);
+    const predictions1 = await runModel(model1, tensor1, false);
     console.timeEnd("model1  prediction" + i);
-    console.timeEnd("pass" + i);
+     console.time("model2  prediction" + i);
+    const predictions2 = await runModel(model2, tensor2, false);
+    console.timeEnd("model2  prediction" + i);
   }
 }
 
-function average(array) {
-    let average = array.reduce((a, b) => a + b) / array.length;
-    return average;
-}
 
 async function benchmarkStichedInputDefininedInCode() {
-   console.log("tf.loadGraphModeling time");
-   console.time("tf.loadGraphModeling times");
-    let models = await Promise.all([
-      tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/seperatemobilenet/model.json"),
-      tf.loadGraphModel("https://storage.googleapis.com/uplara_tfjs/seperatemobilenet2/model.json")
-    ])
-    console.timeEnd("tf.loadGraphModeling times");
-    benchmarkStichedInput(models, 10);
+    console.log("bencharmking stitched input");
+    benchmarkStichedInput(10);
 }
 
 benchmarkStichedInputDefininedInCode();
