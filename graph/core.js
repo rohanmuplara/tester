7058ab85bacc98f5091e8ab2d1b1fe629d7950d1
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-11 04:14:22 -0700

    adding the tensor

--- graph/core.js
+++ graph/core.js
@@ -1,5 +1,6 @@
 async function runModel(model, tensorMap, returnTensorReferences ) {
     let num_outputs = model.outputs.length;
+    debugger;
     let renamedTensorMap = {};
     for (const tensor_name in  tensorMap) {
       const new_tensor_name = tensor_name + ":0"
@@ -22,6 +23,46 @@ async function runModel(model, tensorMap, returnTensorReferences ) {
     }
 }
 
+function convertMaskToColors(tensor) {
+  
+"""
+This takes a raw mask and gives it colors. This is noninituive and little hacking of the api. The params is actually the color array and the mask is indicies
+as each mask has a class(integer) that corresponds to the collars array. The batch size,width,height is from mask 
+and the depth is from the colors array.
+"""
+def convert_mask_colors(mask):
+    colors = tf.tensor(
+        [
+            [0, 0, 0],
+            [255, 0, 0],
+            [32, 173, 10],
+            [117, 112, 2],
+            [136, 10, 117],
+            [34, 16, 169],
+            [36, 121, 142],
+            [248, 109, 67],
+            [242, 124, 242],
+            [208, 97, 48],
+            [49, 220, 181],
+            [216, 210, 239],
+            [27, 50, 31],
+            [206, 173, 55],
+            [127, 98, 97],
+            [255, 229, 85],
+            [234, 123, 143],
+            [122, 52, 124],
+            [242, 68, 46],
+            [68, 34, 91],
+            [34, 236, 26],
+            [236, 26, 84],
+            [94, 0, 96],
+            [63, 35, 198],
+            [110, 103, 165],
+            [105, 245, 12],
+        ]
+    )
+    return tf.gather(colors, mask)
+}
 
 
 
