300efac0a93309b45ab006bd72105e405bc19c69
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-12 20:53:49 -0700

    single and multibenchmark script

--- graph/multi_benchmark.js
+++ graph/multi_benchmark.js
@@ -1,5 +1,5 @@
 async function benchmarkInput (models, num_runs) {
-      cloth_graph_outputs = {"cloth_mask": tf.ones([1,256,193,1]), "cloth": tf.ones([1,256,193,1])}
+      cloth_graph_outputs = {"cloth_mask": tf.ones([1,256,192,1]), "cloth": tf.ones([1,256,192,3])}
       let person = tf.ones([1,256,192,3])
       let person_detection = models["person_detection"]
       let person_detection_output = await runModel(models["person_detection"], {"person": person}, ["person"], true);
@@ -15,7 +15,7 @@ async function benchmarkInput (models, num_runs) {
       console.log("make it pass tps output");
       let cloth_inpainting_output = await runModel(models["cloth_inpainting"], {"warped_cloth": tps_output["warped_cloth"], "warped_cloth_mask":tps_output["warped_cloth_mask"],"cloth": cloth_graph_outputs["cloth"], "expected_seg_mask": expected_seg_output["expected_seg_mask"]}, ["inpainted_cloth"], true);
       console.log("make it pass cloth_inpainting");
-      let skin_inpainting_output = await runModel(models["sking_inpainting"], {"person": human_parsing_output["person"], "human_parsing_mask": human_parsing_output["human_parsing_mask"], "expected_seg_mask": expected_seg_output["expected_seg_mask"], "inpainted_cloth": cloth_inpainting_output["inpainted_cloth"]}, ["person"], true);
+      let skin_inpainting_output = await runModel(models["skin_inpainting"], {"person": human_parsing_output["person"], "human_parsing_mask": human_parsing_output["human_parsing_mask"], "expected_seg_mask": expected_seg_output["expected_seg_mask"], "inpainted_cloth": cloth_inpainting_output["inpainted_cloth"]}, ["person"], true);
       console.log("make it pass skin inpainting");
 }
 
@@ -27,10 +27,10 @@ function average(array) {
 async function benchmarkInputDefininedInCode() {
   let model_paths_dict = {"tps": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json", 
                 "person_detection": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/person_detection_graph/model.json",
-               "denspose":"https://storage.googleapis.com/uplara_tfjs/newest_rohan/denspose_graph/model.json",
+               "denspose":"https://storage.googleapis.com/uplara_tfjs/newest_rohan/denspose_graph2/model.json",
                "human_binary_mask": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_binary_graph/model.json",
                "human_parsing": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_parsing_graph/model.json",
-               "expected_seg": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/expected_seg_graph/model.json",
+               "expected_seg": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/expected_seg_graph2/model.json",
                "tps":  "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json", 
                "cloth_inpainting": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/cloth_inpainting_graph/model.json",
             "skin_inpainting": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/skin_inpainting_graph2/model.json"};
