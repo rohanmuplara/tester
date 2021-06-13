e2f189541c59c84aefa12cab7308cd35684d40fb
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-12 20:44:11 -0700

    more fixes

--- graph/multi_benchmark.js
+++ graph/multi_benchmark.js
@@ -1,15 +1,22 @@
 async function benchmarkInput (models, num_runs) {
+      cloth_graph_outputs = {"cloth_mask": tf.ones([1,256,193,1]), "cloth": tf.ones([1,256,193,1])}
       let person = tf.ones([1,256,192,3])
       let person_detection = models["person_detection"]
       let person_detection_output = await runModel(models["person_detection"], {"person": person}, ["person"], true);
-      let denspose_output = await runModel(models["denspose"], {"person": person_detection_output["person"]}, ["person"], true);
+      let denspose_output = await runModel(models["denspose"], {"person": person_detection_output["person"]}, ["denspose_mask"], true);
       console.log("make it pass denspose output");
-      let human_binary_mask_output = await runModel(models["human_binary_mask"], {"person": person_detection_output["person"], "denspose_mask": denspose_output["denspose_mask"]}, ["human_binary_mask"], false);
-      let human_parsing_output = await runModel(models["human_parsing"], {"person": person_detection_output["person"], "denspose_mask": denspose_output["denspose_mask"], "human_binary_mask":  human_binary_mask_output["human_binary_mask"]}, ["person","human_parsing_mask" ], false);
-      let expected_seg_output = await runModel(models["expected_seg"], {"person": human_parsing_output["person"], "cloth": cloth_graph_outputs["cloth"], "cloth_mask": cloth_graph_outputs["cloth_mask"], "denspose_mask": denspose_output["denspose_mask"], "human_parsing_mask": human_parsing_output["human_parsing_mask"]},["expected_seg_mask"], false);
-      let tps_output = await runModel(models["tps"], {"expected_seg_mask": expected_seg_output["expected_seg_mask"], "cloth": cloth_graph_outputs["cloth"], "cloth_mask": cloth_graph_outputs["cloth_mask"]}, ["warped_cloth", "warped_cloth_mask"], false);
-      let cloth_inpainting_output = await runModel(models["cloth_inpainting"], {"warped_cloth": tps_output["warped_cloth"], "warped_cloth_mask":tps_output["warped_cloth_mask"],"cloth": cloth_graph_outputs["cloth"], "expected_seg_mask": expected_seg_output["expected_seg_mask"]}, ["inpainted_cloth"], false);
-      let skin_inpainting_output = await runModel(models["sking_inpainting"], {"person": human_parsing_output["person"], "human_parsing_mask": human_parsing_output["human_parsing_mask"], "expected_seg_mask": expected_seg_output["expected_seg_mask"], "inpainted_cloth": cloth_inpainting_out["inpainted_cloth"]}, ["person"], false);
+      let human_binary_mask_output = await runModel(models["human_binary_mask"], {"person": person_detection_output["person"], "denspose_mask": denspose_output["denspose_mask"]}, ["human_binary_mask"], true);
+      console.log("make it pass human_binary");
+      let human_parsing_output = await runModel(models["human_parsing"], {"person": person_detection_output["person"], "denspose_mask": denspose_output["denspose_mask"], "human_binary_mask":  human_binary_mask_output["human_binary_mask"]}, ["person","human_parsing_mask" ], true);
+      console.log("make it pass human_paring");
+      let expected_seg_output = await runModel(models["expected_seg"], {"person": human_parsing_output["person"], "cloth": cloth_graph_outputs["cloth"], "cloth_mask": cloth_graph_outputs["cloth_mask"], "denspose_mask": denspose_output["denspose_mask"], "human_parsing_mask": human_parsing_output["human_parsing_mask"]},["expected_seg_mask"], true);
+      console.log("make it pass expected seg");
+      let tps_output = await runModel(models["tps"], {"expected_seg_mask": expected_seg_output["expected_seg_mask"], "cloth": cloth_graph_outputs["cloth"], "cloth_mask": cloth_graph_outputs["cloth_mask"]}, ["warped_cloth", "warped_cloth_mask"], true);
+      console.log("make it pass tps output");
+      let cloth_inpainting_output = await runModel(models["cloth_inpainting"], {"warped_cloth": tps_output["warped_cloth"], "warped_cloth_mask":tps_output["warped_cloth_mask"],"cloth": cloth_graph_outputs["cloth"], "expected_seg_mask": expected_seg_output["expected_seg_mask"]}, ["inpainted_cloth"], true);
+      console.log("make it pass cloth_inpainting");
+      let skin_inpainting_output = await runModel(models["sking_inpainting"], {"person": human_parsing_output["person"], "human_parsing_mask": human_parsing_output["human_parsing_mask"], "expected_seg_mask": expected_seg_output["expected_seg_mask"], "inpainted_cloth": cloth_inpainting_output["inpainted_cloth"]}, ["person"], true);
+      console.log("make it pass skin inpainting");
 }
 
 function average(array) {
@@ -20,8 +27,8 @@ function average(array) {
 async function benchmarkInputDefininedInCode() {
   let model_paths_dict = {"tps": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json", 
                 "person_detection": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/person_detection_graph/model.json",
-               "denspose":"https://storage.googleapis.com/uplara_tfjs/newest_rohan/person_detection_graph/model.json",
-               "human_binary": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_binary_graph/model.json",
+               "denspose":"https://storage.googleapis.com/uplara_tfjs/newest_rohan/denspose_graph/model.json",
+               "human_binary_mask": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_binary_graph/model.json",
                "human_parsing": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_parsing_graph/model.json",
                "expected_seg": "https://storage.googleapis.com/uplara_tfjs/newest_rohan/expected_seg_graph/model.json",
                "tps":  "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json", 
