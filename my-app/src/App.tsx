639eb35a684334dadff25f9a5acb5244a1647f45
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-15 09:17:49 -0700

    more app changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useRef } from "react";
 import { useDropzone } from "react-dropzone";
 import "./App.css";
-import { Tops_Tfjs } from "./tfjs_code/TopsTfjs";
+import { convert_file_to_img } from "./tfjs_code/image_utils";
+import { Tops_Tfjs } from "./tfjs_code/tops_tfjs";
+import * as tf from "@tensorflow/tfjs";
 
 function App() {
   const refContainer = useRef<Tops_Tfjs>();
@@ -10,9 +12,20 @@ function App() {
     refContainer.current = new Tops_Tfjs();
   }, []);
 
-  const onDrop = useCallback((acceptedFiles) => {
+  const onDrop = useCallback(async (acceptedFiles) => {
     console.log("the accepted files are" + acceptedFiles);
-    refContainer.current!.handle_person_upload(acceptedFiles);
+    let cloth_path =
+      "https://storage.googleapis.com/uplara_tfjs/cloth_images/a/cloth_mask_raw.png";
+    let cloth_mask_path =
+      "https://storage.googleapis.com/uplara_tfjs/cloth_images/a/cloth_raw.png";
+    let person_image = await convert_file_to_img(acceptedFiles[0]);
+    let person_tensor = tf.browser.fromPixels(person_image, 3);
+    refContainer.current!.runModel(
+      cloth_path,
+      cloth_mask_path,
+      "dummy_person",
+      person_tensor
+    );
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
