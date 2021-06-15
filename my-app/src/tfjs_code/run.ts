874c4dd1bd28f79e5d7d51d40d424ddfa1e11a50
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-14 21:48:46 -0700

    more changes to run

--- my-app/src/tfjs_code/run.ts
+++ my-app/src/tfjs_code/run.ts
@@ -1,5 +1,4 @@
 import * as tf from "@tensorflow/tfjs";
-import { models } from "@tensorflow/tfjs";
 import {
   drawPixelsToCanvas,
   handle_image_load,
@@ -9,74 +8,64 @@ import {
   convertMaskToColors,
 } from "./core";
 export class End_to_End_Tops {
-  models_map: Map<string, tf.GraphModel>;
+  models_dict: Map<string, tf.GraphModel>;
 
   constructor() {
     this.initializeModel(this.getModelsPathDict());
-    this.models_map = new Map<string, tf.GraphModel>();
   }
-  getModelsPathDict(): Map<string, string> {
-    let model_names_map = new Map<string, string>([
-      [
-        "person_detection",
+  getModelsPathDict(): any {
+    return {
+      person_detection:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/person_detection_graph/model.json",
-      ],
-      [
-        "denspose",
+      denspose:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/denspose_graph2/model.json",
-      ],
-      [
-        "human_binary_mask",
+      human_binary_mask:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_binary_graph/model.json",
-      ],
-      [
-        "human_parsing",
+      human_parsing:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/human_parsing_graph/model.json",
-      ],
-      [
-        "expected_seg",
+      expected_seg:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/expected_seg_graph2/model.json",
-      ],
-      [
-        "tps",
-        "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json",
-      ],
-      [
-        "cloth_inpainting",
+      tps: "https://storage.googleapis.com/uplara_tfjs/newest_rohan/tps_graph/model.json",
+      cloth_inpainting:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/cloth_inpainting_graph/model.json",
-      ],
-      [
-        "skin_inpainting",
+      skin_inpainting:
         "https://storage.googleapis.com/uplara_tfjs/newest_rohan/skin_inpainting_graph2/model.json",
-      ],
-    ]);
-    return model_names_map;
+    };
   }
   async initializeModel(models_paths_dict: Map<string, string>) {
+    let cloth_mask_tensor = await convertMaskUrlToTensor(
+      "https://storage.googleapis.com/uplara_tfjs/cloth_images/a/cloth_mask_raw.png"
+    );
+    cloth_mask_tensor = tf.cast(cloth_mask_tensor, "float32");
+    debugger;
+    this.models_dict = await Promise.all(
+      Object.entries(models_paths_dict).map(
+        async ([model_name, model_path]) => [
+          model_name,
+          await tf.loadGraphModel(model_path),
+        ]
+      )
+    ).then(Object.fromEntries);
+    this.complete_process();
     
-   models_paths_dict.entries().map(model_name: string, model_path:string) => {
-        let model = await tf.loadGraphModel(model_path)
-        return model_name, model_path
-   })
-   
   }
   async person_graph(person: tf.Tensor) {
     person = tf.expandDims(person, 0);
     person = tf.cast(person, "float32");
     let person_detection_output = await runModel(
-      this.models_map.get("person_detection")!,
+      this.models_dict.get("person_detection")!,
       { person: person },
       ["person"],
       true
     );
     let denspose_output = await runModel(
-      this.models_map.get("denspose")!,
+      this.models_dict.get("denspose")!,
       { person: person_detection_output["person"] },
       ["denspose_mask"],
       true
     );
     let human_binary_mask_output = await runModel(
-      this.models_map.get("human_binary_mask")!,
+      this.models_dict.get("human_binary_mask")!,
       {
         person: person_detection_output["person"],
         denspose_mask: denspose_output["denspose_mask"],
@@ -85,7 +74,7 @@ export class End_to_End_Tops {
       true
     );
     let human_parsing_output = await runModel(
-      this.models_map.get("human_parsing")!,
+      this.models_dict.get("human_parsing")!,
       {
         person: person_detection_output["person"],
         denspose_mask: denspose_output["denspose_mask"],
@@ -99,7 +88,7 @@ export class End_to_End_Tops {
 
   async tryon_graph(cloth_graph_outputs: any, person_graph_outputs: any) {
     let expected_seg_output = await runModel(
-      this.models_map.get("expected_seg")!,
+      this.models_dict.get("expected_seg")!,
       {
         person: person_graph_outputs["person"],
         cloth: cloth_graph_outputs["cloth"],
@@ -115,7 +104,7 @@ export class End_to_End_Tops {
       "expected_seg_output.png"
     );
     let tps_output = await runModel(
-      this.models_map.get("tps")!,
+      this.models_dict.get("tps")!,
       {
         expected_seg_mask: expected_seg_output["expected_seg_mask"],
         cloth: cloth_graph_outputs["cloth"],
@@ -126,7 +115,7 @@ export class End_to_End_Tops {
     );
     drawPixelsToCanvas(tps_output["warped_cloth"], "warped_cloth.png");
     let cloth_inpainting_output = await runModel(
-      this.models_map.get("cloth_inpainting")!,
+      this.models_dict.get("cloth_inpainting")!,
       {
         warped_cloth: tps_output["warped_cloth"],
         warped_cloth_mask: tps_output["warped_cloth_mask"],
@@ -137,7 +126,7 @@ export class End_to_End_Tops {
       true
     );
     let skin_inpainting_output = await runModel(
-      this.models_map.get("skin_inpainting")!,
+      this.models_dict.get("skin_inpainting")!,
       {
         person: person_graph_outputs["person"],
         human_parsing_mask: person_graph_outputs["human_parsing_mask"],
@@ -188,3 +177,5 @@ export class End_to_End_Tops {
     );
   }
 }
+
+
