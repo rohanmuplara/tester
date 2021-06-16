4433bf6902148e74d2101fb0f32a935da62e7181
Rohan Mahajan <rohanmahajan1993@gmail.com>
2021-06-16 09:30:11 -0700

    existing keys

---
+++ my-app/src/tfjs_code/storage_map.ts
@@ -0,0 +1,29 @@
+export class Storage_Map {
+  existing_keys: string[];
+  namespace: string;
+  constructor(namespace: string) {
+    this.namespace = namespace;
+    let metaTablePath = namespace + ":metadata";
+    if (localStorage.getItem(metaTablePath) === null) {
+      this.existing_keys = [];
+    } else {
+      this.existing_keys = JSON.parse(localStorage.getItem(namespace)!);
+    }
+  }
+  setItem(key: string, value: any): void {
+    let namespaced_key = this.namespace + ":" + key;
+    localStorage.setItem(namespaced_key, value);
+    this.existing_keys.unshift(key);
+    let metaTablePath = this.namespace + ":metadata";
+    localStorage.setItem(metaTablePath, JSON.stringify(this.existing_keys));
+  }
+  getItem(key: string): any {
+    let namespaced_key = this.namespace + ":" + key;
+    let json_value = localStorage.getItem(namespaced_key);
+    return JSON.parse(json_value!);
+  }
+
+  getExistingKeys(): string[] {
+    return this.existing_keys;
+  }
+}
